d5fe345e350a2551df70248d8fa82df4bd59bef8
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-27 12:13:25 +0600

    added a function for plus button

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // plus button
 document.getElementById('plusButton').addEventListener('click', function () {
-  valueCount = document.getElementById('inputValue').value;
+  plusButon('inputValue');
+
+  totalPrice();
+});
+
+function plusButon(id) {
+  valueCount = document.getElementById(id).value;
   valueCount++;
-  document.getElementById('inputValue').value = valueCount;
+  document.getElementById(id).value = valueCount;
 
   if (valueCount > 0) {
     document.getElementById('minusButton').removeAttribute('disabled');
     document.getElementById('minusButton').classList.remove('disabled');
   }
-
-  totalPrice();
-});
+}
 
 // minus button
 
