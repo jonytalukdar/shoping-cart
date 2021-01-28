d2df6d5d26a415229d1c16ef1159aba111ddc1e5
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-28 17:03:31 +0600

    added another function for phone

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function handleProductChange(isIncrease) {
-  const caseInput = document.getElementById('case-count');
+function handleProductChange(isIncrease, count, total) {
+  const caseInput = document.getElementById(count);
   const caseCount = parseInt(caseInput.value);
   let caseNewCount = 0;
   if (isIncrease == true) {
@@ -10,5 +10,22 @@ function handleProductChange(isIncrease) {
   }
   caseInput.value = caseNewCount;
   const caseTotal = caseNewCount * 59;
-  document.getElementById('case-total').innerText = caseTotal;
+  document.getElementById(total).innerText = caseTotal;
+}
+
+function handlePhoneChange(increase) {
+  const phoneInput = document.getElementById('phone-count');
+  const phoneCount = parseInt(phoneInput.value);
+  //   const phoneNewCount = phoneCount - 1;
+  let phoneNewCount = 0;
+  if (increase == true) {
+    phoneNewCount = phoneCount + 1;
+  }
+  if (increase == false && phoneCount > 0) {
+    phoneNewCount = phoneCount - 1;
+  }
+  phoneInput.value = phoneNewCount;
+
+  const phoneTotal = phoneNewCount * 1219;
+  document.getElementById('phone-total').innerText = phoneTotal;
 }
